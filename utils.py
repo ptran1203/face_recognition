f41da2b885cc4e92bb22163ed4004c713964c3f8
phattran <phat.tranhoang@cyberlogitec.com>
2020-10-22 13:21:21 +0700

    plt

--- utils.py
+++ utils.py
@@ -2,13 +2,21 @@ import pickle
 import numpy as np
 import urllib.request
 import keras.preprocessing.image as image_processing
+import matplotlib.pyplot as plt
 import cv2
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 try:
     from google.colab.patches import cv2_imshow
 except ImportError:
     from cv2 import imshow as cv2_imshow
 
 MEAN_PIXCELS = np.array([103.939, 116.779, 123.68])
+DECOMPOSERS = {
+    'pca': PCA(),
+    'tsne': TSNE()
+}
+
 def pickle_save(object, path):
     try:
         print('save data to {} successfully'.format(path))
@@ -98,3 +106,55 @@ def http_get_img(url, rst=64, gray=False, normalize=True):
         img = norm(preprocess(img))
 
     return img
+
+
+def visualize_scatter_with_images(X_2d_data, images, figsize=(10,10), image_zoom=0.5):
+    fig, ax = plt.subplots(figsize=figsize)
+    artists = []
+    for xy, i in zip(X_2d_data, images):
+        x0, y0 = xy
+        img = OffsetImage(i, zoom=image_zoom)
+        ab = AnnotationBbox(img, (x0, y0), xycoords='data', frameon=False)
+        artists.append(ax.add_artist(ab))
+    ax.update_datalim(X_2d_data)
+    ax.autoscale()
+    plt.show()
+
+
+def visualize_scatter(data_2d, label_ids, figsize=(8,8), legend=True,title="None"):
+    plt.figure(figsize=figsize)
+    plt.grid()
+    
+    nb_classes = len(np.unique(label_ids))
+    colors = cm.rainbow(np.linspace(0, 1, nb_classes))
+
+    for i,label_id in enumerate(np.unique(label_ids)):
+        plt.scatter(data_2d[np.where(label_ids == label_id), 0],
+                    data_2d[np.where(label_ids == label_id), 1],
+                    marker='o',
+                    color=colors[i],
+                    linewidth='1',
+                    alpha=0.8,
+                    label=label_id)
+    if legend:
+        plt.legend(loc='best')
+    else:
+        # plt.title(title)
+        plt.axis('off')
+
+    plt.show()
+
+
+def scatter_plot(x, y, encoder, name='chart', opt='pca', plot_img=None,
+                legend=True, title="None"):
+    step = 1
+    if encoder.input_shape[-1] != x.shape[-1]:
+        x = triple_channels(x)
+
+    x_embeddings = encoder.predict(x)
+    if len(x_embeddings.shape) > 2:
+        x_embeddings = x_embeddings.reshape(x_embeddings.shape[0], -1)
+    decomposed_embeddings = decomposers[opt].fit_transform(x_embeddings)
+    if plot_img:
+        return visualize_scatter_with_images(decomposed_embeddings,x)
+    visualize_scatter(decomposed_embeddings, y, legend=legend,title=title)
